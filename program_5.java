5ec95af74ed7b5bf358461735c97cc9b6e65170d
UtkarshShankar <53379005+UtkarshShankar@users.noreply.github.com>
2019-10-16 02:18:52 +0530

    Update program_5.java

--- program_5.java
+++ program_5.java
@@ -1,27 +1,24 @@
 package Project_euler;
 
-public class program_5 {
+public class program_5 
+{
 
-	public static void main(String[] args) {
-
-		int i=1,j,c=0,a=0;
-		
-		for(i=21;i<1000000000;i++)
+	static int gcd(int a,int b)
+	{
+		if(a==0)
 		{
-			for(j=1;j<=20;j++)
-			{
-				c=i%j;
-				if(c==0)
-					a++;
-			}
-			if(a==20)
-			{
-				System.out.println(i);
-				break;
-             }
-			a=0;
-		}
-           
+		return b;
+	    }
+		return gcd(b%a,a);
 	}
-
+	
+	public static void main(String args[])
+	{
+	int result=1;
+	for(int i=1;i<20;i++)
+	{
+		result=(result*i)/gcd(result,i);
+	}
+	System.out.println(result);
+	}
 }
